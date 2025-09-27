0c3df8775a2105edafad8210b16941bcb6b742e9
jeevabala <jeevabala@gmail.com>
2025-09-27 06:41:10 -0400

    Handled AddToCart button after removal

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -13,6 +13,7 @@ function ProductList({ onHomeClick }) {
     const dispatch = useDispatch();
     const CartItems = useSelector(state => state.cart.items);
 
+
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -254,12 +255,26 @@ function ProductList({ onHomeClick }) {
         e.preventDefault();
         setShowPlants(true); // Set showAboutUs to true when "About Us" link is clicked
         setShowCart(false); // Hide the cart when navigating to About Us
+
+        handleAddToCartButton();
     };
 
     const handleContinueShopping = (e) => {
         e.preventDefault();
         setShowCart(false);
+
+        handleAddToCartButton();
+        
     };
+
+    const handleAddToCartButton = () => {
+        setAddedToCart((prevState) => ({}));
+        CartItems.map((item) => {
+            setAddedToCart((prevState) => ({
+                ...prevState, [item.name]:true,
+            }));
+        });
+    }
 
     const handleAddToCart = (product) => {
         dispatch(addItem(product));
@@ -273,6 +288,8 @@ function ProductList({ onHomeClick }) {
     };
 
     const totalQuantity = calculateTotalQuantity();
+
+
 
     return (
         <div>
@@ -297,16 +314,16 @@ function ProductList({ onHomeClick }) {
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg>
                                 {totalQuantity > 0 && (
                                     <span className='cart_quantity_count'
-                                        // style={{
-                                        //     position: 'absolute',
-                                        //     top: '0px',
-                                        //     right: '0px',
-                                        //     background: 'red',
-                                        //     color: 'white',
-                                        //     borderRadius: '50%',
-                                        //     padding: '4px 8px',
-                                        //     fontSize: '14px',
-                                        // }}
+                                    // style={{
+                                    //     position: 'absolute',
+                                    //     top: '0px',
+                                    //     right: '0px',
+                                    //     background: 'red',
+                                    //     color: 'white',
+                                    //     borderRadius: '50%',
+                                    //     padding: '4px 8px',
+                                    //     fontSize: '14px',
+                                    // }}
                                     >
                                         {totalQuantity}
                                     </span>)}
